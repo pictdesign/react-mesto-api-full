efd325c6941b871f4f75bf1fe898369e77385f7a
pictdesign <ask@pict.design>
2022-07-16 23:58:00 +0300

    add: cors

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const userRouter = require('./router/users');
 const cardsRouter = require('./router/cards');
 const NotFoundError = require('./errors/NotFoundError');
+const cors = require('./middlewares/cors');
 const { login, createUser } = require('./controllers/users');
 const { isAuthorized } = require('./middlewares/auth');
 const { validateUrl } = require('./utils/utils');
@@ -14,20 +15,28 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cookieParser());
-app.use(requestLogger);
-
 mongoose.connect('mongodb://localhost:27017/mesto', {
   useNewUrlParser: true,
 });
 
+app.use(cookieParser());
+app.use(requestLogger);
+app.use(cors);
 app.use(bodyParser.json());
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -37,6 +46,7 @@ app.post('/signup', celebrate({
     avatar: Joi.string().custom(validateUrl, 'validate'),
   }),
 }), createUser);
+
 app.use(isAuthorized, userRouter);
 app.use(isAuthorized, cardsRouter);
 
---
+++ backend/middlewares/cors.js
@@ -0,0 +1,29 @@
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const allowedCors = [
+  'localhost:3000',
+  'http://localhost:3000',
+  'pictdesign.nomoredomains.xyz/',
+  'http://pictdesign.nomoredomains.xyz/',
+  'https://pictdesign.nomoredomains.xyz/',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+  return true;
+};
