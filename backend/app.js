895a655eb8debb30f2bf0c910010980e80f7c103
pictdesign <ask@pict.design>
2022-07-17 17:09:09 +0300

    fix: bodyparser

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ app.use(Cors);
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
